0088c69bd00a7e01cd6f9dcd8dfd7aef5a52a103
paoliml <kyungeun_95@naver.com>
2019-05-14 00:21:25 +0900

    Initial commit

---
+++ app/src/main/java/com/example/paoliml/isori/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.paoliml.isori;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    public Button but1;
+    public void init(){
+        but1=(Button)findViewById(R.id.loc_conf);
+        but1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toy = new Intent(MainActivity.this, loc_configuration.class);
+                startActivity(toy);
+            }
+        });
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+}
+
---
+++ app/src/main/java/com/example/paoliml/isori/loc_configuration.java
@@ -0,0 +1,22 @@
+package com.example.paoliml.isori;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class loc_configuration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loc_configuration);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+    }
+
+}
