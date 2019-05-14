20c0ce4cf5652862266648b79eb27beca6786b81
paoliml <kyungeun_95@naver.com>
2019-05-14 18:08:46 +0900

    .

--- app/src/main/java/com/example/paoliml/isori/MainActivity.java
+++ app/src/main/java/com/example/paoliml/isori/MainActivity.java
@@ -11,8 +11,19 @@ public class MainActivity extends AppCompatActivity {
 
     public Button but1;
     public void init(){
-        but1=(Button)findViewById(R.id.loc_conf);
+        but1=(Button)findViewById(R.id.button3);
         but1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toy = new Intent(MainActivity.this, listsafe.class);
+                startActivity(toy);
+            }
+        });
+    }
+    public Button but1_1;
+    public void init1(){
+        but1_1=(Button)findViewById(R.id.loc_conf);
+        but1_1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent toy = new Intent(MainActivity.this, loc_configuration.class);
@@ -20,11 +31,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
+        init1();
     }
 }
 
